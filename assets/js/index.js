95a75f57d42637716af5c654c98dad05d727c6cc
tazpowell <hellotazuko@gmail.com>
2018-09-02 16:07:40 -0400

    Refactor columns
    - Add additional wrapper div around case-study-box so that column widths
      remain equal even when there are odd number of columns
      (last odd column was showing as full-width)
    - Need to update 'case-study-item' name to be less confusing with
      'case-study-box'

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,5 @@
 'use strict'
 
-// const events = require('./forecasts/events')
-
 $(() => {
   $('#caseStudyFilter').on('click', onFilterCaseStudies)
 
@@ -10,19 +8,17 @@ $(() => {
 const onFilterCaseStudies = function (event) {
   event.preventDefault()
   console.log('event is ', event)
-  const selectAll = $('#showcase').find('.case-study-box')
-  const selectEvens = $('#showcase').find('.case-study-box:odd')
-  const selectOdds = $('#showcase').find('.case-study-box:even')
+  const selectAll = $('#showcase').find('.case-study-item')
+  const selectEvens = $('#showcase').find('.case-study-item:odd')
+  const selectOdds = $('#showcase').find('.case-study-item:even')
 
   if (event.target.id === "showAll"){
-    console.log('event.target is showAll')
     selectAll.show()
     $('#showAll').addClass('active')
     $('#showEven').removeClass('active')
     $('#showOdd').removeClass('active')
   }
   else if (event.target.id === "showEven"){
-    console.log('event.target is showEven')
     selectEvens.show()
     selectOdds.hide()
     $('#showAll').removeClass('active')
@@ -30,7 +26,6 @@ const onFilterCaseStudies = function (event) {
     $('#showOdd').removeClass('active')
   }
   else if (event.target.id === "showOdd"){
-    console.log('event.target is showOdd')
     selectOdds.show()
     selectEvens.hide()
     $('#showAll').removeClass('active')
@@ -38,5 +33,4 @@ const onFilterCaseStudies = function (event) {
     $('#showOdd').addClass('active')
   }
 
-  // debugger
 }
